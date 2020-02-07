03efae02a30412000669c16ddb08d1cb02f22094
fankaiming2 <1043046221@qq.com>
2020-02-08 01:06:56 +0800

    AAAfkm10430

--- src/main/java/com/atguigu/jianfa1205/Dugujiujian1205.java
+++ src/main/java/com/atguigu/jianfa1205/Dugujiujian1205.java
@@ -3,8 +3,6 @@ package com.atguigu.jianfa1205;
 public class Dugujiujian1205 {
     public static void main(String[] args) {
         System.out.println("hello___------_Dugujiujian");
-        System.out.println("hello___------_Dugujiujian");
-        System.out.println("hello___------_Dugujiujian");
         System.out.println("AAAAAAAAAAA");
 
     }
