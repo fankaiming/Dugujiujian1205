7e692a007eb6a2f095bba00e8558a213bccc4b30
fankaiming2 <1043046221@qq.com>
2020-02-08 01:24:31 +0800

    BBBBBB

--- src/main/java/com/atguigu/jianfa1205/Dugujiujian1205.java
+++ src/main/java/com/atguigu/jianfa1205/Dugujiujian1205.java
@@ -4,6 +4,6 @@ public class Dugujiujian1205 {
     public static void main(String[] args) {
         System.out.println("hello___------_Dugujiujian");
         System.out.println("AAAAAAAAAAA");
-
+        System.out.println("BBBBBBB");
     }
 }
